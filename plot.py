daa8dab5fd80d0c4a07331c9786bc4774e68e6b8
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2025-09-07 18:29:05 +0200

    Added plot script

---
+++ plot.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+import json
+from os.path import isfile
+from statistics import mean
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+BENCHMARKS = {
+    "datamining": {
+        "benchmarks": ["correlation", "covariance"],
+        "nrows": 1,
+        "ncols": 2
+    },
+    "linear-algebra/blas": {
+        "benchmarks": ["gemm", "gemver", "gesummv", "symm", "syr2k", "syrk", "trmm"],
+        "nrows": 2,
+        "ncols": 4
+    },
+    "linear-algebra/kernels": {
+        "benchmarks": ["2mm", "3mm", "atax", "bicg", "doitgen", "mvt"],
+        "nrows": 2,
+        "ncols": 3
+    },
+    "linear-algebra/solvers": {
+        "benchmarks": ["cholesky", "durbin", "gramschmidt", "lu", "ludcmp", "trisolv"],
+        "nrows": 2,
+        "ncols": 3
+    },
+    "medley": {
+        "benchmarks": ["deriche", "floyd-warshall", "nussinov"],
+        "nrows": 1,
+        "ncols": 3
+    },
+    "stencils": {
+        "benchmarks": ["adi", "fdtd-2d", "heat-3d", "jacobi-1d", "jacobi-2d", "seidel-2d"],
+        "nrows": 2,
+        "ncols": 3
+    }
+}
+WIDTH=6.4
+HEIGHT=4.8
+
+results = {}
+
+def plot_benchmark(versions: dict[str, dict[str, str]], ref: str, group: str, benchmark: str, ax = None) -> None:
+    if ax is None:
+        ax = plt.gca()
+    bench_path = f"{group}/{benchmark}"
+    x = []
+    y = []
+    colors = []
+    edgecolors = []
+    hatches = []
+    x.append(versions[ref]["name"])
+    y.append(1.0)
+    ref_time = mean(results[ref][bench_path]["data"])
+    colors.append(versions[ref]["color1"])
+    edgecolors.append(versions[ref]["color1"])
+    hatches.append("")
+    for version, version_data in versions.items():
+        if version == ref:
+            continue
+        x.append(version_data["name"])
+        if results[version][bench_path]["status"] in ["good", "unstable"]:
+            y.append(mean(results[version][bench_path]["data"]) / ref_time)
+            if results[version][bench_path]["status"] == "good":
+                colors.append(version_data["color1"])
+                edgecolors.append(version_data["color1"])
+                hatches.append("")
+            else:
+                colors.append("#ffffff")
+                edgecolors.append(version_data["color2"])
+                hatches.append("/")
+        else:
+            y.append(0.0)
+            colors.append("#ffffff")
+            hatches.append("")
+    ax.grid(axis="y", zorder=0)
+    nodata_y = max(y) * 0.1
+    for bar in ax.bar(x, y, color=colors, edgecolor=edgecolors, hatch=hatches, zorder=3):
+        if bar.get_height() == 0.0:
+            ax.text(bar.get_x() + 0.4, nodata_y, "x", fontsize="xx-large", fontweight="bold", horizontalalignment="center", zorder=4)
+    ax.set(ylabel="Normalized runtime", title=benchmark)
+
+def plot_group(versions: dict[str, dict[str, str]], ref: str, group: str) -> None:
+    nrows = BENCHMARKS[group]["nrows"]
+    ncols = BENCHMARKS[group]["ncols"]
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, squeeze=False, figsize=(WIDTH * ncols, HEIGHT * nrows), height_ratios=[HEIGHT] * nrows)
+    bench = 0
+    for row in range(nrows):
+        for col in range(ncols):
+            if bench >= len(BENCHMARKS[group]["benchmarks"]):
+                fig.delaxes(axs[row][col])
+            else:
+                plot_benchmark(versions, ref, group, BENCHMARKS[group]["benchmarks"][bench], axs[row][col]) # type: ignore
+                bench += 1
+    #fig.suptitle(group, fontsize="x-large", fontweight="demi")
+    fig.subplots_adjust(bottom=(0.15 / nrows))
+    patches = []
+    patches.append(mpatches.Patch(color=versions[ref]["color1"], label=versions[ref]["name"]))
+    for version, version_data in versions.items():
+        if version != ref:
+            patches.append(mpatches.Patch(color=version_data["color1"], label=version_data["name"]))
+    fig.legend(handles=patches, loc="lower center", ncols=len(patches))
+    outfile = "plots/" + group.replace("/", "_") + ".png"
+    fig.savefig(outfile)
+
+def plot_all(versions: dict[str, dict[str, str]], ref: str) -> None:
+    if not ref in versions:
+        print(f"{ref} not in versions")
+        exit(1)
+    for version in versions.keys():
+        file_path = f"results/{version}.json"
+        if not isfile(file_path):
+            print(f"File does not exist: {file_path}")
+            exit(1)
+        data = None
+        with open(file_path, "r") as file:
+            data = json.load(file)
+        if data == None:
+            print(f"Could not load JSON from file: {file_path}")
+            exit(1)
+        results[version] = data
+    for group in BENCHMARKS.keys():
+        plot_group(versions, ref, group)
+
+if __name__ == "__main__":
+    VERSIONS = {
+        "ref": {
+            "name": "ref",
+            "color1": "#7a7a7a",
+            "color2": "#a8a8a8"
+        },
+        "opt_c": {
+            "name": "optimized (C)",
+            "color1": "#cc1c2f",
+            "color2": "#ff7078"
+        },
+        "intel": {
+            "name": "intel",
+            "color1": "#0072b4",
+            "color2": "#79abe2"
+        }
+    }
+    REF = "ref"
+    from os import path, makedirs
+    if not path.exists("plots/"):
+        makedirs("plots")
+    plot_all(VERSIONS, REF)
